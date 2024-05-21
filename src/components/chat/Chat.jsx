9f0b826f164ebe98e693bcd35485c9948e36328c
Saiful Ajom Khan <sajomkhan@gmail.com>
2024-05-22 00:48:22 +0600

    Updated Chat Component

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,92 @@
-import "./chat.css"
+import { useState } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
-  return (
-    <div className="chat">Chat</div>
-  )
-}
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
 
-export default Chat
+  console.log(text);
+
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
+
+  return (
+    <div className="chat">
+      {/* ============= Top Div ============== */}
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor veniam?</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      {/* ============= Center Div ============== */}
+      <div className="center">
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum sapiente laborum alias ex, quas obcaecati? Officia ea dolore aliquid quia nesciunt voluptatum quisquam sunt eum enim. Voluptatem aperiam cum fuga.</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum sapiente laborum alias ex, quas obcaecati? Officia ea dolore aliquid quia nesciunt voluptatum quisquam sunt eum enim. Voluptatem aperiam cum fuga.</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="https://p0.pxfuel.com/preview/440/625/255/cosmos-flower-cosmos-plant-pink-flowers-pictures-of-flowers-royalty-free-thumbnail.jpg" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum sapiente laborum alias ex, quas obcaecati? Officia ea dolore aliquid quia nesciunt voluptatum quisquam sunt eum enim. Voluptatem aperiam cum fuga.</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum sapiente laborum alias ex, quas obcaecati? Officia ea dolore aliquid quia nesciunt voluptatum quisquam sunt eum enim. Voluptatem aperiam cum fuga.</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+      {/* ============= Bottom Div ============== */}
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button>Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
